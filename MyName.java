c695e4363f07a3ede7e9b111c8b42fd27d01086b
Safvan <safvanpmcz@gmail.com>
2022-07-23 17:41:28 +0530

    4 replaced successfully

--- MyName.java
+++ MyName.java
@@ -11,7 +11,7 @@ public class MyName
             for (int colmn=1;colmn<=n;colmn++ )
             {
                 if ( row==1 || row==(n/2+1) || row ==n ||
-                        (colmn==1 && row!=4) || (colmn ==n && row!=2)
+                        (colmn==1 && row!=(n-1)) || (colmn ==n && row!=2)
                 )
                 {
                     System.out.print("$");
@@ -31,7 +31,7 @@ public class MyName
             for(int colmn=1;colmn<=row;colmn++)
             {
                 if (row > (n/2+1) && colmn == 2 || row > (n/2+1) && colmn == (n/2+1) || // 2nd & (n/2+1)ird columns not required for both rows
-                        row == n && colmn == 4      // for nth row
+                        row == n && colmn == (n-1)      // for nth row
                 )
                     System.out.print("  ");
                 else
@@ -95,7 +95,7 @@ public class MyName
             for(int colmn=1;colmn<=row;colmn++)
             {
                 if (row > (n/2+1) && colmn == 2 || row > (n/2+1) && colmn == (n/2+1) || // 2nd & (n/2+1)ird columns not required for both rows
-                        row == n && colmn == 4      // for nth row
+                        row == n && colmn == (n-1)      // for nth row
                 )
                     System.out.print("  ");
                 else
@@ -128,7 +128,7 @@ public class MyName
 			// ********* to print a Separator Dot between name and Initial  *********************** *
             for (int column=1;column <=n;column++)
             {
-                if (row == 4 && (column == 2 || column == (n/2+1) )   ||
+                if (row == (n-1) && (column == 2 || column == (n/2+1) )   ||
                         row == n && (column == 2 || column == (n/2+1) )
                 )
                     System.out.print("$");
@@ -140,7 +140,7 @@ public class MyName
             // ********* to print columns with  $ Symbol  for Letter P  *********************** *
             for (int column=1;column <=n;column++)
             {
-                if ( row==1 || row == (n/2+1) || column == 1 || (column == n && row !=4 && row != n ))
+                if ( row==1 || row == (n/2+1) || column == 1 || (column == n && row !=(n-1) && row != n ))
                     System.out.print("$ ");
                 else
                     System.out.print("  ");
