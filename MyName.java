d47e229690859a0f437a304a730a16cbf82752b6
Safvan <safvanpmcz@gmail.com>
2022-07-23 17:20:21 +0530

    3 replaced successfully

--- MyName.java
+++ MyName.java
@@ -10,7 +10,7 @@ public class MyName
             // ********* to print columns with  $ Symbol  for Letter S  *********************** *
             for (int colmn=1;colmn<=n;colmn++ )
             {
-                if ( row==1 || row==3 || row ==n ||
+                if ( row==1 || row==(n/2+1) || row ==n ||
                         (colmn==1 && row!=4) || (colmn ==n && row!=2)
                 )
                 {
@@ -30,7 +30,7 @@ public class MyName
             // to print  $ Symbol
             for(int colmn=1;colmn<=row;colmn++)
             {
-                if (row > 3 && colmn == 2 || row > 3 && colmn == 3 || // 2nd & 3ird columns not required for both rows
+                if (row > (n/2+1) && colmn == 2 || row > (n/2+1) && colmn == (n/2+1) || // 2nd & (n/2+1)ird columns not required for both rows
                         row == n && colmn == 4      // for nth row
                 )
                     System.out.print("  ");
@@ -51,7 +51,7 @@ public class MyName
 			// ********* to print columns with  $ Symbol  for Letter F  *********************** *
             for (int colmn=1;colmn<=n;colmn++ )
             {
-                if ( row == 1 || row ==3 || colmn ==1)
+                if ( row == 1 || row ==(n/2+1) || colmn ==1)
                     System.out.print("$ ");
                 else
                     System.out.print("  ");
@@ -94,7 +94,7 @@ public class MyName
             // to print  $ Symbol
             for(int colmn=1;colmn<=row;colmn++)
             {
-                if (row > 3 && colmn == 2 || row > 3 && colmn == 3 || // 2nd & 3ird columns not required for both rows
+                if (row > (n/2+1) && colmn == 2 || row > (n/2+1) && colmn == (n/2+1) || // 2nd & (n/2+1)ird columns not required for both rows
                         row == n && colmn == 4      // for nth row
                 )
                     System.out.print("  ");
@@ -128,8 +128,8 @@ public class MyName
 			// ********* to print a Separator Dot between name and Initial  *********************** *
             for (int column=1;column <=n;column++)
             {
-                if (row == 4 && (column == 2 || column == 3 )   ||
-                        row == n && (column == 2 || column == 3 )
+                if (row == 4 && (column == 2 || column == (n/2+1) )   ||
+                        row == n && (column == 2 || column == (n/2+1) )
                 )
                     System.out.print("$");
                 else
@@ -140,7 +140,7 @@ public class MyName
             // ********* to print columns with  $ Symbol  for Letter P  *********************** *
             for (int column=1;column <=n;column++)
             {
-                if ( row==1 || row == 3 || column == 1 || (column == n && row !=4 && row != n ))
+                if ( row==1 || row == (n/2+1) || column == 1 || (column == n && row !=4 && row != n ))
                     System.out.print("$ ");
                 else
                     System.out.print("  ");
